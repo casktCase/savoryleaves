baa4ad16adf6f7117c0dd61e31baa09142ffda62
casktCase <caseyrudge@gmail.com>
2019-02-24 09:27:01 -0700

    additions to style in responsive layout

--- js/start.js
+++ js/start.js
@@ -21,4 +21,15 @@ skel.init({
             containers: 1200
         }
     }
-});
+}, {
+        overlays: {
+            titleBar: {
+                breakpoints: 'mobile',
+                position: 'top-left',
+                height: 44,
+                width: '100%',
+                html: '<span class="toggle" data-action="togglePanel" data-args="navPanel"></span>' +
+                    '<span class="title" data-action="copyHTML" data-args="headerimg"></span>'
+            }
+        }
+    });
